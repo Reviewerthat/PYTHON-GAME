a2a4dbc55a6cdb647dbe32cd68349106587ab06e
Reviewerthat <reviewerthat@gmail.com>
2025-04-07 18:03:32 +0100

    Update tkinterl2.py

--- tkinterl1.py
+++ tkinterl1.py
@@ -1,11 +1,31 @@
 from tkinter import *
-root = Tk()
+win = Tk()
+win.geometry("600x500") #or minsize
+win.title("Log In Layout")
+win.config(background="orange")
+#lables for username and pass
+username = Label(win,text="Username",
+                    bg = "purple").place(x =60, y=60)
 
-root.geometry("500x600")
-btn = Button(root,text = "Click me",background="blue",bd = 10,activeforeground="pink",activebackground="green",command = root.destroy)
+password = Label(win,text="Password",bg = "yellow").place(x=60, y=90)
 
-btn.pack(side="top")
+# input area / entrylabels
+username_input_area = Entry(win,width = 20).place(x=140,y=60)
+user_password_entry_area = Entry(win,width=20,show="*").place(x=140, y=90)
+
+#submit button
+btn = Button(win,text="Submit",bg="red",
+                bd = 1).place(x=60,y=130)
 
 
 
-root.mainloop()
+
+
+
+
+
+
+
+
+
+win.mainloop()
